958a5e16196a2fc7b10804b4cca40d62ba92369e
tantida <92627203+tantida@users.noreply.github.com>
2021-11-20 17:44:17 +0700

    Start the SDK

--- app/src/main/java/com/sqisland/android/hello/MainActivity.java
+++ app/src/main/java/com/sqisland/android/hello/MainActivity.java
@@ -2,12 +2,16 @@ package com.sqisland.android.hello;
 
 import android.app.Activity;
 import android.os.Bundle;
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
 
 public class MainActivity extends Activity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-    AppCenter.start(getApplication(), "{Your App Secret}", Analytics.class, Crashes.class);
+    AppCenter.start(getApplication(), "b007f239-99d8-492b-a682-215b0d075456",
+                  Analytics.class, Crashes.class);
   }
 }
